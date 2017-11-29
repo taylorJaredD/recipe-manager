dbe86ae67fa5247758a61861265f12c003220b24
Jared <jaredtaylor80@gmail.com>
2017-11-29 16:47:40 -0500

    working on user association

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,10 +12,10 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
-    # if @recipe.user != current.user
-    #   flash[:alert] = "Only the author of this recipe can edit it."
-    #   redirect_to recipe_path(@recipe)
-    # end
+    if @recipe.user != current.user
+      flash[:alert] = "Only the author of this recipe can edit it."
+      redirect_to recipe_path(@recipe)
+    end
   end
 
   def new
@@ -34,23 +34,23 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
-    # if @recipe.user == current.user
-    #   @recipe.update(recipe_params)
-    # else
-    #   flash[:alert] = "Only the author of this recipe can edit it."
-    # end
-    @recipe.update(recipe_params)
+    if @recipe.user == current.user
+      @recipe.update(recipe_params)
+    else
+      flash[:alert] = "Only the author of this recipe can edit it."
+    end
+    # @recipe.update(recipe_params)
     redirect_to recipe_path(@recipe)
   end
 
   def destroy
     @recipe = Recipe.find(params[:id])
-    # if @recipe.user == current.user
-    #   @recipe.destroy
-    # else
-    #   flash[:alert] = "Only the author of this recipe can delete it."
-    # end
-    @recipe.destroy
+    if @recipe.user == current.user
+      @recipe.destroy
+    else
+      flash[:alert] = "Only the author of this recipe can delete it."
+    end
+    # @recipe.destroy
     redirect_to recipes_path
   end
 
