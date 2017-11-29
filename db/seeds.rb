ccccc6b27abea137a77fa1dc73e904c6358b8069
Jared <jaredtaylor80@gmail.com>
2017-11-29 13:47:40 -0500

    started adding css styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,17 @@ Ingredient.destroy_all
 Recipe.destroy_all
 
 test_user = User.create(email: "test1@test.com", password: "password")
+grammy = User.create(email: "grammybakery@sweet.treats", password: "sweettooth")
 
-recipes = Recipe.create([
-  {title: "Devil's Food Cake", img_url: "", description: "Delicious chocolatey cake", directions: "melt chocolate. beat eggs", bake_time: "30-35 minutes", baking_method: "9x9 square pan at 350 degrees", serving_size: "preferably more than one", user_id: 1}
-  ])
+recipe = Recipe.create([
+  {title: "Devil's Food Cake", img_url: "", description: "Delicious chocolatey cake", directions: "melt chocolate. beat eggs", bake_time: "30-35 minutes", baking_method: "9x9 square pan at 350 degrees", serving_size: "preferably more than one", user_id: test_user.id},
+  {title: "", img_url: "", description: "", deirections: "", bake_time: "", baking_method: "", serving_size: "", user_id: grammy.id},
+
+])
 
 ingredients = Ingredient.create([
   {amount: "1 1/2", measurement: "cups", item: "flour", recipe_id: 1},
   {amount: "3", measurement: "", item: "eggs", recipe_id: 1},
-  {amount: "1", measurement: "lbs", item: "chocolate", recipe_id: 1}
+  {amount: "1", measurement: "lbs", item: "chocolate", recipe_id: 1},
+  
   ])
